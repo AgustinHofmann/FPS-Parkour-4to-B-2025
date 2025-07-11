400050deb936bfe4a65c68e1599ea5a274e8653b
Agustin Hofmann <agustinhofmann@gmail.com>
2025-07-11 10:26:33 -0300

    TERMINADO

--- Assets/EnemyObject.cs
+++ Assets/EnemyObject.cs
@@ -7,7 +7,18 @@ public class EnemyObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthManager = FindObjectOfType<HealthManager>();
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (healthManager.HealthUpdate(damage))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     // Update is called once per frame
